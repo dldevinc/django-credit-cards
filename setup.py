2dabd76de88cf7b41d8517cafe10ed54f6e95030
pix <pix666@ya.ru>
2019-01-09 12:00:27 +0400

    Bump to v0.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/dldevinc/django-credit-cards'
 EMAIL = 'x896321475@gmail.com'
 AUTHOR = 'Mihail Mishakin'
 REQUIRES_PYTHON = '>=3'
-VERSION = '0.3'
+VERSION = '0.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
